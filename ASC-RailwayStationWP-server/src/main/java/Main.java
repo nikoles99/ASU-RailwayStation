a9e83fd28521f8663eb9516c675a0c55a3744f28
nikoles99 <nikido@tut.by>
2016-12-22 11:34:28 +0300

    first commit

---
+++ ASC-RailwayStationWP-server/src/main/java/Main.java
@@ -0,0 +1,20 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class Main {
+
+    @RequestMapping("/index")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Main.class, args);
+    }
+}
