dd56123e152b2341854aed7b89c8af05f635355b
nikoles99 <nikido@tut.by>
2016-12-22 16:48:47 +0300

    send test json

--- ASC-RailwayStationWP-server/src/main/java/Main.java
+++ ASC-RailwayStationWP-server/src/main/java/Main.java
@@ -1,18 +1,31 @@
+import bean.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.AbstractJsonpResponseBodyAdvice;
+
 
 @Controller
 @EnableAutoConfiguration
 public class Main {
 
-    @RequestMapping("/index")
+    @RequestMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    String home() {
-        return "Hello World!";
+    User home(User user1) {
+        User user = new User();
+        user.setLogin("qwer");
+        user.setPassword("zxc");
+        return user;
     }
+    @ControllerAdvice
+    public class JsonpAdvice extends AbstractJsonpResponseBodyAdvice {
+        public JsonpAdvice() {
+            super("callback");
+        }
+    }
+
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Main.class, args);
