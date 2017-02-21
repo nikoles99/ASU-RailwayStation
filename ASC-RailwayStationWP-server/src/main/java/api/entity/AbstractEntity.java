784f073a3a4f46cc9f75bba224d673b7ce9d66b4
nikoles99 <nikido@tut.by>
2017-02-21 16:07:16 +0300

    JPA Logging on

--- ASC-RailwayStationWP-server/src/main/java/api/entity/AbstractEntity.java
+++ ASC-RailwayStationWP-server/src/main/java/api/entity/AbstractEntity.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 public class AbstractEntity {
     @Id
     @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     public AbstractEntity() {
