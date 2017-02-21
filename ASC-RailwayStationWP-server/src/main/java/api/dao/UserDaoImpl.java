278f49ab7cef387fc8c18b4ff8beb6e0fe649018
nikoles99 <nikido@tut.by>
2017-02-21 16:16:17 +0300

    JPA Logging on

--- ASC-RailwayStationWP-server/src/main/java/api/dao/UserDaoImpl.java
+++ ASC-RailwayStationWP-server/src/main/java/api/dao/UserDaoImpl.java
@@ -3,27 +3,24 @@ package api.dao;
 import api.entity.UserEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
 
+@Transactional(readOnly = false, propagation = Propagation.REQUIRES_NEW)
 @Repository
 public class UserDaoImpl implements UserDao {
 
     private static final Logger logger = LoggerFactory.getLogger(UserDaoImpl.class);
 
-    @Autowired
-    private EntityManagerFactory entityManagerFactory;
-
-    private EntityManager getEntityManagerFactory() {
-        return entityManagerFactory.createEntityManager();
-    }
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Override
     public void addUser(UserEntity userEntity) {
-        EntityManager entityManager = getEntityManagerFactory();
         entityManager.persist(userEntity);
         logger.info("User add successfully " + userEntity);
     }
