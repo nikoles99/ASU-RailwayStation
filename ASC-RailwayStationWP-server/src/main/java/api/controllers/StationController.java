86722eae5c3870557400189e654c2c033c02d906
nikoles99 <nikido@tut.by>
2017-03-01 15:50:06 +0300

    fixed order.html

--- ASC-RailwayStationWP-server/src/main/java/api/controllers/StationController.java
+++ ASC-RailwayStationWP-server/src/main/java/api/controllers/StationController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
 /**
  * Created by nikita on 27.02.17.
  */
