26b530b23cf1dff2892d54d39b912cd465f67b9e
nikoles99 <nikido@tut.by>
2017-03-09 15:41:26 +0300

    add administration functionality

--- ASC-RailwayStationWP-server/src/main/java/api/controllers/AbstractController.java
+++ ASC-RailwayStationWP-server/src/main/java/api/controllers/AbstractController.java
@@ -1,12 +1,14 @@
 package api.controllers;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.mvc.method.annotation.AbstractJsonpResponseBodyAdvice;
 
 /**
  * Created by nikita on 28.02.17.
  */
 
+@CrossOrigin
 public class AbstractController {
 
     @ControllerAdvice
--- ASC-RailwayStationWP-server/src/main/java/api/controllers/UserController.java
+++ ASC-RailwayStationWP-server/src/main/java/api/controllers/UserController.java
@@ -4,8 +4,10 @@ import api.model.UserBean;
 import api.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
@@ -14,9 +16,9 @@ public class UserController extends AbstractController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/login")
     @ResponseBody
-    UserBean home(@RequestBody final UserBean userBean) {
+    UserBean addUser(@RequestBody final UserBean userBean) {
         userService.addUser(userBean);
         return userBean;
     }
