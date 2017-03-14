00ea2dcdef56128fc261f8fea6a6b5923183da96
nikita <nikido@tut.by>
2017-03-14 23:59:38 +0300

    add services

--- ASC-RailwayStationWP-server/src/main/java/api/dao/passenger/PassengerDaoImpl.java
+++ ASC-RailwayStationWP-server/src/main/java/api/dao/passenger/PassengerDaoImpl.java
@@ -2,10 +2,15 @@ package api.dao.passenger;
 
 import api.dao.AbstractDao;
 import api.entity.PassengerEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by nikita on 14.03.17.
  */
+@Transactional(propagation = Propagation.REQUIRES_NEW)
+@Repository
 public class PassengerDaoImpl extends AbstractDao<PassengerEntity> implements PassengerDao {
     @Override
     public void addPassenger(PassengerEntity passengerEntity) {
--- ASC-RailwayStationWP-server/src/main/java/api/dao/schedule/TrainScheduleDaoImpl.java
+++ ASC-RailwayStationWP-server/src/main/java/api/dao/schedule/TrainScheduleDaoImpl.java
@@ -2,10 +2,15 @@ package api.dao.schedule;
 
 import api.dao.AbstractDao;
 import api.entity.TrainScheduleEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by nikita on 14.03.17.
  */
+@Transactional(propagation = Propagation.REQUIRES_NEW)
+@Repository
 public class TrainScheduleDaoImpl extends AbstractDao<TrainScheduleEntity> implements TrainScheduleDao {
 
     @Override
