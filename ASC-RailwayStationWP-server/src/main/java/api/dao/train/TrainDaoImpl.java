8a3ce15b32603874c9cd4ff771cc6636db660661
nikoles99 <nikido@tut.by>
2017-03-17 14:38:39 +0300

    add fixes TrainDaoImpl

--- ASC-RailwayStationWP-server/src/main/java/api/dao/train/TrainDaoImpl.java
+++ ASC-RailwayStationWP-server/src/main/java/api/dao/train/TrainDaoImpl.java
@@ -63,26 +63,24 @@ public class TrainDaoImpl extends AbstractDao<TrainEntity> implements TrainDao {
         Join<TrainEntity, TrainScheduleEntity> trainScheduleJoin = trainEntityRoot.join("scheduleEntities");
         Join<TrainScheduleEntity, StationEntity> stationEntityJoin = trainScheduleJoin.join("stationEntity");
 
-        List<Predicate> conditions = new ArrayList<Predicate>();
-
-        Predicate arrivalStationEqualsPredicate = criteriaBuilder.equal(stationEntityJoin.get("name"), arrivalStation);
-        conditions.add(arrivalStationEqualsPredicate);
-        Predicate departureDateEqualsPredicate = criteriaBuilder.equal(stationEntityJoin.get("name"), departureStation);
-        conditions.add(departureDateEqualsPredicate);
         Date arrivalDate = DateUtils.format(arrivalDateStr);
-        Predicate arrivalDatePredicate = criteriaBuilder.equal(trainScheduleJoin.get("id"), 86);
-        conditions.add(arrivalDatePredicate);
         Date departureDate = DateUtils.format(departureDateStr);
-        Predicate departureDatePredicate = criteriaBuilder.equal(trainScheduleJoin.get("id"), 85);
-        conditions.add(departureDatePredicate);
 
+        Predicate arrivalStationEquals = criteriaBuilder.equal(stationEntityJoin.get("name"), arrivalStation);
+        Predicate departureDateEqualsPredicate = criteriaBuilder.equal(stationEntityJoin.get("name"), departureStation);
+
+        Predicate arrivalDateLessArrivalDate = criteriaBuilder.lessThanOrEqualTo(trainScheduleJoin.get("arrivalDate").as(Date.class), arrivalDate);
+        Predicate arrivalDateGreaterDepartureDate = criteriaBuilder.greaterThanOrEqualTo(trainScheduleJoin.get("arrivalDate").as(Date.class), departureDate);
+        Predicate arrivalDateGroup = criteriaBuilder.and(arrivalStationEquals, arrivalDateLessArrivalDate, arrivalDateGreaterDepartureDate);
+
+        Predicate departureDateGreaterDepartureDate = criteriaBuilder.greaterThanOrEqualTo(trainScheduleJoin.get("departureDate").as(Date.class), departureDate);
+        Predicate departureDateLessArrivalDate = criteriaBuilder.lessThanOrEqualTo(trainScheduleJoin.get("departureDate").as(Date.class), arrivalDate);
+        Predicate departureDateGroup = criteriaBuilder.and(departureDateEqualsPredicate, departureDateGreaterDepartureDate, departureDateLessArrivalDate);
+
+        Predicate result = criteriaBuilder.or(arrivalDateGroup, departureDateGroup);
         TypedQuery<TrainEntity> typedQuery = getEntityManager().createQuery(criteriaQuery
                 .select(trainEntityRoot)
-                .where(conditions.get(0))
-                .where(conditions.get(1))
-                .where(conditions.get(2))
-                .where(conditions.get(3))
-                .orderBy(criteriaBuilder.asc(trainEntityRoot.get("name")))
+                .where(result)
                 .distinct(true)
         );
         return typedQuery.getResultList();
