fbf2882b01c341574eec7f050be0fa3cbfc10270
nikoles99 <nikido@tut.by>
2017-03-22 16:11:31 +0300

    get Trains By route

--- ASC-RailwayStationWP-server/src/main/java/api/dao/train/TrainDaoImpl.java
+++ ASC-RailwayStationWP-server/src/main/java/api/dao/train/TrainDaoImpl.java
@@ -5,10 +5,6 @@ import api.entity.StationEntity;
 import api.entity.TrainEntity;
 import api.entity.TrainScheduleEntity;
 import api.utils.DateUtils;
-import org.hibernate.criterion.DetachedCriteria;
-import org.hibernate.criterion.Projections;
-import org.hibernate.criterion.Restrictions;
-import org.hibernate.criterion.Subqueries;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
@@ -57,37 +53,6 @@ public class TrainDaoImpl extends AbstractDao<TrainEntity> implements TrainDao {
         return getTrainsByParams(arrivalStation, departureStation, arrivalDate, departureDate);
     }
 
-  /*  @Override
-    public List<TrainEntity> getTrainsByParams(String arrivalStation, String departureStation, Date arrivalDate, Date departureDate) {
-        CriteriaBuilder criteriaBuilder = getEntityManager().getCriteriaBuilder();
-        CriteriaQuery<TrainEntity> criteriaQuery = criteriaBuilder.createQuery(TrainEntity.class);
-
-        Root<TrainEntity> trainEntityRoot = criteriaQuery.from(TrainEntity.class);
-
-        Join<TrainEntity, TrainScheduleEntity> trainScheduleJoin = trainEntityRoot.join("scheduleEntities");
-        Join<TrainScheduleEntity, StationEntity> stationEntityJoin = trainScheduleJoin.join("stationEntity");
-
-        Predicate arrivalStationEquals = criteriaBuilder.equal(stationEntityJoin.get("name"), arrivalStation);
-        Predicate departureDateEqualsPredicate = criteriaBuilder.equal(stationEntityJoin.get("name"), departureStation);
-
-        Predicate arrivalDateLessArrivalDate = criteriaBuilder.lessThanOrEqualTo(trainScheduleJoin.get("arrivalDate").as(Date.class), arrivalDate);
-        Predicate arrivalDateGreaterDepartureDate = criteriaBuilder.greaterThanOrEqualTo(trainScheduleJoin.get("arrivalDate").as(Date.class), departureDate);
-        Predicate arrivalDateGroup = criteriaBuilder.and(arrivalStationEquals, arrivalDateLessArrivalDate, arrivalDateGreaterDepartureDate);
-
-        Predicate departureDateGreaterDepartureDate = criteriaBuilder.greaterThanOrEqualTo(trainScheduleJoin.get("departureDate").as(Date.class), departureDate);
-        Predicate departureDateLessArrivalDate = criteriaBuilder.lessThanOrEqualTo(trainScheduleJoin.get("departureDate").as(Date.class), arrivalDate);
-        Predicate departureDateGroup = criteriaBuilder.and(departureDateEqualsPredicate, departureDateGreaterDepartureDate, departureDateLessArrivalDate);
-
-        Predicate result = criteriaBuilder.or(arrivalDateGroup, departureDateGroup);
-        TypedQuery<TrainEntity> typedQuery = getEntityManager().createQuery(criteriaQuery
-                .select(trainEntityRoot)
-                .where(result)
-                .distinct(true)
-        );
-        return typedQ
-        uery.getResultList();
-    }*/
-
     @Override
     public List<TrainEntity> getTrainsByParams(String arrivalStation, String departureStation, Date arrivalDate, Date departureDate) {
         CriteriaBuilder criteriaBuilder = getEntityManager().getCriteriaBuilder();
@@ -96,44 +61,36 @@ public class TrainDaoImpl extends AbstractDao<TrainEntity> implements TrainDao {
         Root<TrainEntity> trainEntityRoot = criteriaQuery.from(TrainEntity.class);
 
         Join<TrainEntity, TrainScheduleEntity> trainScheduleJoin = trainEntityRoot.join("scheduleEntities");
-        Join<TrainScheduleEntity, StationEntity> stationEntityJoin1 = trainScheduleJoin.join("stationEntity");
+        Join<TrainScheduleEntity, StationEntity> stationEntityJoin = trainScheduleJoin.join("stationEntity");
 
-
-        Subquery<Date> subQuery = criteriaQuery.subquery(Date.class);
-
-        Root<TrainEntity> arrivalStationArrivalDate = subQuery.from(TrainEntity.class);
-        Join<TrainEntity, Date> trainScheduleJoinDate = arrivalStationArrivalDate.join("scheduleEntities");
-        Join<TrainScheduleEntity, StationEntity> stationEntityJoin = trainScheduleJoinDate.join("stationEntity");
-
-        Predicate arrivalStationPredicate = criteriaBuilder.equal(stationEntityJoin.get("name"), arrivalStation);
-        Predicate trainIdPredicate = criteriaBuilder.equal(arrivalStationArrivalDate.get("id"), trainEntityRoot.get("id"));
-        Predicate re = criteriaBuilder.and(arrivalStationPredicate, trainIdPredicate);
-
-
-
+        Subquery<Date> subQuery = getDateSubQuery(arrivalStation, criteriaBuilder, criteriaQuery, trainEntityRoot);
 
         Predicate departureDateLessThan = criteriaBuilder.lessThan(trainScheduleJoin.get("departureDate").as(Date.class), subQuery);
-        Predicate departureStationPredicate = criteriaBuilder.equal(stationEntityJoin1.get("name"), departureStation);
-        Predicate arrivalDatPredicate = criteriaBuilder.lessThanOrEqualTo(trainScheduleJoin.get("arrivalDate").as(Date.class), arrivalDate);
-        Predicate departureDatePredicate = criteriaBuilder.greaterThanOrEqualTo(trainScheduleJoin.get("departureDate").as(Date.class), departureDate);
+        Predicate departureStationPredicate = criteriaBuilder.equal(stationEntityJoin.get("name"), departureStation);
+        Predicate result = criteriaBuilder.and(departureDateLessThan, departureStationPredicate);
 
-        Predicate result = criteriaBuilder.and(departureStationPredicate, departureDateLessThan);
-
-        subQuery.select(trainScheduleJoinDate).where(re);
 
         criteriaQuery.select(trainEntityRoot).where(result);
-
         TypedQuery<TrainEntity> resultQuery = getEntityManager().createQuery(criteriaQuery);
-
-
         return resultQuery.getResultList();
     }
 
-}
 
-/*
-select * from "trains" t
-        inner join "train_schedules" s on t.id = s.train_id
-        join "stations" st on st.id = s.station_id where st.name = 'Гродно' and s.departure_date < (select s.arrival_date from "trains" t1
-        inner join "train_schedules" s on t1.id = s.train_id
-        join "stations" st on st.id = s.station_id where st.name = 'Молодечно' and t1.id = t.id)*/
+    private Subquery<Date> getDateSubQuery(String arrivalStation, CriteriaBuilder criteriaBuilder,
+                                           CriteriaQuery<TrainEntity> criteriaQuery, Root<TrainEntity> trainEntityRoot) {
+        Subquery<Date> subQuery = criteriaQuery.subquery(Date.class);
+
+        Root<TrainEntity> subQueryRoot = subQuery.from(TrainEntity.class);
+        Join<TrainEntity, TrainScheduleEntity> subQueryTrainScheduleJoin = subQueryRoot.join("scheduleEntities");
+        Join<TrainScheduleEntity, StationEntity> subQueryStationEntityJoin = subQueryTrainScheduleJoin.join("stationEntity");
+
+
+        Predicate arrivalStationPredicate = criteriaBuilder.equal(subQueryStationEntityJoin.get("name"), arrivalStation);
+        Predicate trainIdPredicate = criteriaBuilder.equal(subQueryRoot.get("id"), trainEntityRoot.get("id"));
+        Predicate resultSubQuery = criteriaBuilder.and(arrivalStationPredicate, trainIdPredicate);
+
+        Expression<Date> arrivalDateExpression = subQueryTrainScheduleJoin.get("arrivalDate").as(Date.class);
+        subQuery.select(arrivalDateExpression).where(resultSubQuery);
+        return subQuery;
+    }
+}
