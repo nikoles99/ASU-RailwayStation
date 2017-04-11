706975ae0508355711bc3929c6be993bd00cf33e
nikoles99 <nikido@tut.by>
2017-04-11 16:55:41 +0300

    start adding security

--- ASC-RailwayStationWP-server/src/main/java/api/dao/impl/RoleDaoImpl.java
+++ ASC-RailwayStationWP-server/src/main/java/api/dao/impl/RoleDaoImpl.java
@@ -3,12 +3,17 @@ package api.dao.impl;
 import api.dao.AbstractDao;
 import api.dao.RoleDao;
 import api.entity.RoleEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 /**
  * Created by nolesuk on 11-Apr-17.
  */
+@Repository
+@Transactional(propagation = Propagation.MANDATORY)
 public class RoleDaoImpl extends AbstractDao<RoleEntity> implements RoleDao {
     @Override
     public RoleEntity getById(Integer id) {
