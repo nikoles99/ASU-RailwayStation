09d5dffbb042ae5642bd0daa45b56aef40c379e6
nikita <nikido@tut.by>
2017-04-15 18:32:31 +0300

    add authentication

--- ASC-RaileayStationWP-client/src/script/authentication.js
+++ ASC-RaileayStationWP-client/src/script/authentication.js
@@ -2,6 +2,7 @@
  * Created by nolesuk on 13-Apr-17.
  */
 var NOT_FOUND_USER = "No entity found for query; nested exception is javax.persistence.NoResultException: No entity found for query";
+var BAD_CREDENTIALS = "Bad credentials";
 function authentication(login, password) {
     var url = "http://localhost:8080/authentication";
     return $.ajax({
@@ -15,6 +16,9 @@ function authentication(login, password) {
             if (NOT_FOUND_USER === message) {
                 $('#errorMessage').text("Пользователь c логином \"" + login + "\" не найден");
                 $('#registration').modal();
+            }
+            if (BAD_CREDENTIALS === message) {
+                alert("Неверный логин или пароль");
             }
         }
     });
--- ASC-RaileayStationWP-client/src/script/utils/loggingUtils.js
+++ ASC-RaileayStationWP-client/src/script/utils/loggingUtils.js
@@ -2,11 +2,16 @@
  * Created by nikita on 18.03.17.
  */
 function errorLogging(xhr) {
-    alert(xhr.responseJSON.message);
+    var error = xhr.responseJSON;
+    if (error != null) {
+        error = error.message;
+    } else {
+        error = xhr.statusText;
+    }
+    alert(error);
     console.log("code: " + xhr.code);
     console.log("readyState: " + xhr.readyState);
     console.log("responseText: " + xhr.responseText);
     console.log("status: " + xhr.status);
-    console.log("text status: " + textStatus);
-    console.log("error: " + err);
+    console.log("text status: " + xhr.textStatus);
 }
