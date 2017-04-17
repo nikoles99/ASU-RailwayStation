53b22fe8708d12d10cc0d912ed33d23d8a3f7ad2
nikoles99 <nikido@tut.by>
2017-04-17 18:16:43 +0300

    start booking place

--- ASC-RaileayStationWP-client/src/script/choose_place.js
+++ ASC-RaileayStationWP-client/src/script/choose_place.js
@@ -4,11 +4,12 @@
 const ADULTS = "adults";
 const CHILDREN_WITH_NO_SEATS = "children_with_no_seats";
 const CHILDREN_WITH_SEATS = "children_with_seats";
-var countPassenger = 3;
+var countPassenger;
 var chosenPlaces = [];
 
 function fillForm() {
     chosenPlaces = [];
+    $('#places').prop('disabled',true);
     var freePlaces = $(this).closest('a');
     var trainName = freePlaces.data("trainName");
     var trainId = freePlaces.data("trainId");
@@ -44,8 +45,37 @@ $("#trains").on("click", ".freePlaces", function () {
 });
 
 $(document).on("click", "#continue", function () {
-    $("#last_name").val(data.lastName);
+    var isValidate = validateBookForm();
+    if (isValidate) {
+        bookPlaces();
+    }
 });
+
+$(document).on("click", "#childCount", function () {
+    countPassenger = $("#adultsCount").val() + $("#childCount").val();
+});
+
+$(document).on("click", "#adultsCount", function () {
+    countPassenger = $("#adultsCount").val() + $("#childCount").val();
+});
+
+function validateBookForm() {
+    var adultsCount = $("#adultsCount").val();
+    var childCount = $("#childCount").val();
+    if (adultsCount < 1 || adultsCount > 2) {
+        alert('Можно выбрать только до 2х полных билетов');
+        return false;
+    }
+    if (childCount < 0 || childCount > 3) {
+        alert('Можно выбрать только до 3х детских билетов');
+        return false;
+    }
+    if (chosenPlaces.length != countPassenger) {
+        alert('Число быбранных билетов не соответствует количеству пассажиров');
+        return false;
+    }
+    return true;
+}
 
 function isPlaceContains(place) {
     for (var i = 0; i <= chosenPlaces.length; i++) {
