530f3d9a85561a5f3673707d21e6c13c2ef3551b
nikita <nikido@tut.by>
2017-04-19 00:07:20 +0300

    add cabinet

--- ASC-RaileayStationWP-client/src/script/cabinet.js
+++ ASC-RaileayStationWP-client/src/script/cabinet.js
@@ -21,27 +21,29 @@ function setTickets() {
     var ticketsPromise = getTickets();
     ticketsPromise.then(function (tickets) {
         for (var i = 0; i < tickets.length; i++) {
-            var placeNumber = tickets[i].placeNumber;
+            var departureStation = tickets[i].departureStation;
+            var arrivalStation = tickets[i].arrivalStation;
+            var departureDate = tickets[i].departureDate;
+            var arrivalDate = tickets[i].arrivalDate;
             var carriageType = tickets[i].carriageType;
             var carriageNumber = tickets[i].carriageNumber;
-            var arrivalDate = tickets[i].arrivalDate;
-            var departureDate = tickets[i].departureDate;
-            var arrivalStation = tickets[i].arrivalStation;
-            var departureStation = tickets[i].departureStation;
-            var id = place.carriageId;
-            if (carriageId != "" && carriageId === id) {
-                continue;
-            }
-            var placesColumn = getPlacesByCarriageId(places, id);
-            var carriagePromise = getCarriageById(id);
-            carriagePromise.then(function (carriage) {
-                var tr = "<tr>" +
-                    "<td>" + carriage.number + "</td>" +
-                    "<td>" + placesColumn + "</td>" +
-                    "</tr>";
-                $("#free_places").append(tr);
-            });
-            carriageId = id;
+            var placeNumber = tickets[i].placeNumber;
+            var tr = "<tr>" +
+                "<td>" + departureStation + "</td>" +
+                "<td>" + arrivalStation + "</td>" +
+                "<td>" + departureDate + "</td>" +
+                "<td>" + arrivalDate + "</td>" +
+                "<td>" + carriageType + "</td>" +
+                "<td>" + carriageNumber + "</td>" +
+                "<td>" + placeNumber + "</td>" +
+                "<td><input class=\".btn-danger cancelBooking\" type=\"button\" value=\" Отменить бронирование\" ></td>" +
+                "</tr>";
+            $("#tickets").append(tr);
         }
     });
 }
+
+$(document).on("click", ".btn-danger.cancelBooking", function () {
+
+
+});
